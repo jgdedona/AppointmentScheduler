f2923ea9c3f22784a95eedb2746a8589ddd7279e
LabUser <jdedona@mail.usf.edu>
2022-03-22 15:07:13 -0400

    AddAppointment and MainMenu views

--- src/controller/AddAppointmentController.java
+++ src/controller/AddAppointmentController.java
@@ -7,27 +7,13 @@ import javafx.fxml.Initializable;
 import java.net.URL;
 import java.util.ResourceBundle;
 
-public class MainMenuController implements Initializable {
+public class AddAppointmentController implements Initializable {
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        
-    }
-
-    @FXML
-    void displayAppointments(ActionEvent event) {
 
     }
 
-    @FXML
-    void displayCustomers(ActionEvent event) {
-
-    }
-
-    @FXML
-    void displayReports(ActionEvent event) {
-
-    }
 
 
 }
